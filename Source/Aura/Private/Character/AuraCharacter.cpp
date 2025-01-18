75ad1a34b6880ee18b27da047988c46cdbc74c3d
iegad <iegad@vip.qq.com>
2025-01-19 02:24:58 +0800

    update

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,19 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	UCharacterMovementComponent* characterMovement = GetCharacterMovement();
+	check(characterMovement);
+
+	characterMovement->bOrientRotationToMovement = true;
+	characterMovement->RotationRate = FRotator(0.f, 400.f, 0.f);
+	characterMovement->bConstrainToPlane = true;
+	characterMovement->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,6 +4,7 @@
 #include "Player/AuraPlayerController.h"
 
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -27,4 +28,29 @@ AAuraPlayerController::BeginPlay()
 	inputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	inputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(inputModeData);
+}
+
+void 
+AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* enhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	enhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+}
+
+void 
+AAuraPlayerController::Move(const FInputActionValue& InputActionValue) 
+{
+	const FVector2D inputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator rotation = GetControlRotation();
+	const FRotator yawRotation(0.f, rotation.Yaw, 0.f);
+
+	const FVector forwardDirection = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::X);
+	const FVector rightDirection = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::Y);
+
+	APawn* controllerPawn = GetPawn<APawn>();
+	if (IsValid(controllerPawn)) {
+		controllerPawn->AddMovementInput(forwardDirection, inputAxisVector.Y);
+		controllerPawn->AddMovementInput(rightDirection, inputAxisVector.X);
+	}
 }
