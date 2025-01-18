0a9c75e2d429630a5424632bcd7d31ae7d518e6b
iegad <iegad@vip.qq.com>
2025-01-18 17:19:02 +0800

    update

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	this->bReplicates = true;
+}
+
+void 
+AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(subsystem);
+	subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI inputModeData;
+	inputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	inputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(inputModeData);
+}
