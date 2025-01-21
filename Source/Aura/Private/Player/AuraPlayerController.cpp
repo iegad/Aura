443dcc298437ec3c0ca7a7dfbefb3b9429347702
iegad <iegad@vip.qq.com>
2025-01-22 00:20:49 +0800

    鼠标悬停高亮敌人

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,10 +5,18 @@
 
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	this->bReplicates = true;
+}
+
+void 
+AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	CursorTrace();
 }
 
 void 
@@ -52,5 +60,56 @@ AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	if (IsValid(controllerPawn)) {
 		controllerPawn->AddMovementInput(forwardDirection, inputAxisVector.Y);
 		controllerPawn->AddMovementInput(rightDirection, inputAxisVector.X);
+	}
+}
+
+void 
+AAuraPlayerController::CursorTrace()
+{
+	FHitResult cursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, cursorHit);
+	if (!cursorHit.bBlockingHit) {
+		return;
+	}
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(cursorHit.GetActor());
+
+	// 鼠标的射线追踪, 会出现多种情况:
+	//  1, LastActor == NULL && ThisActor == NULL
+	//		- 什么都不作
+	//  2, LastActor == NULL && ThisActor != NULL
+	//		- ThisActor高亮
+	//  3, LastActor != NULL && ThisActor == NULL
+	//		- LastActor 取消高亮
+	//  4, LastActor != NULL && ThisActor != NULL && ThisActor != LastActor
+	//		- ThisActor 高亮, LastActor 取消高亮
+	//	4, LastActor != NULL && ThisActor != NULL && ThisActor == LastActor
+	//		- 什么都不作
+
+	if (!LastActor) {
+		if (!ThisActor) {
+			// Case 1
+		}
+		else {
+			// Case 2
+			ThisActor->HighlightActor();
+		}
+	}
+	else {
+		if (!ThisActor) {
+			// Case 3
+			LastActor->UnHightlightActor();
+		}
+		else {
+			if (ThisActor != LastActor) {
+				// Case 4
+				LastActor->UnHightlightActor();
+				ThisActor->HighlightActor();
+			}
+			else {
+				// Case 5
+			}
+		}
 	}
 }
