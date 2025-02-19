696339c2b77784958b7c8e3b266d2b3d1245be10
iegad <iegad@vip.qq.com>
2025-02-19 15:27:45 +0800

    update

--- Source/Aura/Private/AbilitySystem/ModMagCalc/MCC_MaxMana.cpp
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MCC_MaxMana.cpp
@@ -8,10 +8,15 @@
 
 UMCC_MaxMana::UMCC_MaxMana()
 {
-	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	// 需要捕获的是 Intelligence 属性
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();	
+	// 属性源, 只有两个 GE本身所属目标或是应用目标, 这里设置为Target 表示 MCC所在GE的应用对象, 即AuraCharacter
 	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	// 快照是用来回档的
 	IntelligenceDef.bSnapshot = false;
 
+	// 要捕获的与计算相关的属性
+	//  添加到该数组中, IntelligenceDef将会被得理
 	RelevantAttributesToCapture.Add(IntelligenceDef);
 }
 
@@ -21,14 +26,18 @@ UMCC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& S
 	const FGameplayTagContainer* sourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
 	const FGameplayTagContainer* targetTags = Spec.CapturedSourceTags.GetAggregatedTags();
 
+	// 用于计算的参数结构
 	FAggregatorEvaluateParameters evaluationParameters;
 	evaluationParameters.SourceTags = sourceTags;
 	evaluationParameters.TargetTags = targetTags;
 
 	float intelligence = 0.f;
-	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, evaluationParameters, intelligence);
-	intelligence = FMath::Max<float>(intelligence, 0.f);
 
+	// 在GE被应用获取 intelligence 值
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, evaluationParameters, intelligence);
+	intelligence = FMath::Max(intelligence, 0.f);
+
+	// 获取角色等级
 	auto* combatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
 	const int32 playerLevel = combatInterface->GetPlayerLevel();
 
--- Source/Aura/Public/AbilitySystem/ModMagCalc/MCC_MaxMana.h
+++ Source/Aura/Public/AbilitySystem/ModMagCalc/MCC_MaxMana.h
@@ -20,5 +20,6 @@ public:
 	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
 
 private:
+	// 定义捕获数据的结构体
 	FGameplayEffectAttributeCaptureDefinition IntelligenceDef;
 };
